01172853e66e48d0bc71210d4c8b8f93daede871
Vinh Pham <85326677+p-vinh@users.noreply.github.com>
2024-11-27 15:21:02 -0800

    Update openChestConfiguration.lua
    Updated to vanilla loot table

--- config/openChestConfiguration.lua
+++ config/openChestConfiguration.lua
@@ -1,30 +1,14 @@
 return {
     { name = "Dragon egg", count = 1, id = 'minecraft:dragon_egg', dmg = 0, chance = 8, color = 0xF6290C },
-
     { name = "Nether star", count = 1, id = 'minecraft:nether_star', dmg = 0, chance = 15, color = 0xEFE80A },
     { name = "Wither Head", count = 1, id = 'minecraft:skull', dmg = 1, chance = 15, color = 0xEFE80A },
-    { name = "Some item", count = 1, id = 'ThaumicTinkerer:kamiResource', dmg = 0, chance = 15, color = 0xEFE80A },
     { name = "Human head", count = 1, id = 'minecraft:skull', dmg = 3, chance = 15, color = 0xEFE80A },
-
-    { name = "Draconium ingot", count = 4, id = 'DraconicEvolution:draconiumIngot', dmg = 0, chance = 20, color = 0xD70AEF },
-    { name = "Draconium dust", count = 4, id = 'DraconicEvolution:draconiumDust', dmg = 0, chance = 20, color = 0xD70AEF },
-
-    { name = "Alchemy block", count = 4, id = 'Thaumcraft:blockMetalDevice', dmg = 3, chance = 20, color = 0x0AEFE5 },
-    { name = "Alchemy bench?", count = 4, id = 'Thaumcraft:blockMetalDevice', dmg = 9, chance = 30, color = 0x0AEFE5 },
-
-    { name = "Nitor", count = 16, id = 'Thaumcraft:ItemResource', dmg = 1, chance = 30, color = 0x131315 },
-    { name = "Alimentum", count = 16, id = 'Thaumcraft:ItemResource', dmg = 0, chance = 30, color = 0x131315 },
-    { name = "Void Metal", count = 8, id = 'Thaumcraft:ItemResource', dmg = 16, chance = 30, color = 0x131315 },
     { name = "Ghast tear", count = 32, id = 'minecraft:ghast_tear', dmg = 0, chance = 30, color = 0x131315 },
-    { name = "some fragment", count = 16, id = 'Thaumcraft:ItemResource', dmg = 9, chance = 30, color = 0x131315 },
     { name = "Ender pearl", count = 16, id = 'minecraft:ender_pearl', dmg = 0, chance = 30, color = 0x131315 },
-    { name = "Balance crystal", count = 24, id = 'Thaumcraft:ItemShard', dmg = 6, chance = 30, color = 0x131315 },
-    { name = "Cluster", count = 32, id = 'Thaumcraft:blockCrystal', dmg = 6, chance = 30, color = 0x131315 },
     { name = "Emerald", count = 16, id = 'minecraft:emerald', dmg = 0, chance = 40, color = 0x131315 },
     { name = "Diamond", count = 16, id = 'minecraft:diamond', dmg = 0, chance = 40, color = 0x131315 },
     { name = "Redstone block", count = 24, id = 'minecraft:redstone_block', dmg = 0, chance = 50, color = 0x131315 },
     { name = "Coal block", count = 16, id = 'minecraft:coal_block', dmg = 0, chance = 50, color = 0x131315 },
-    { name = "Thaum ingot", count = 32, id = 'Thaumcraft:ItemResource', dmg = 2, chance = 40, color = 0x131315 },
     { name = "Iron ingot", count = 32, id = 'minecraft:iron_ingot', dmg = 0, chance = 45, color = 0x131315 },
     { name = "Gold ingot", count = 32, id = 'minecraft:gold_ingot', dmg = 0, chance = 40, color = 0x131315 },
     { name = "Nether wart", count = 16, id = 'minecraft:nether_wart', dmg = 0, chance = 50, color = 0x131315 },
