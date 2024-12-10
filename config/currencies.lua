76d4e34028fcef236b4b7cdb6f82a58c88b1059f
Vinh Pham <85326677+p-vinh@users.noreply.github.com>
2024-12-10 02:03:51 -0800

    Update currencies.lua

--- config/currencies.lua
+++ config/currencies.lua
@@ -10,11 +10,9 @@ local function addCurrency(name, id, dmg, model, color, max)
     })
 end
 
-addCurrency("Money",           "customnpcs:npcMoney",      0,   'INGOT', 0x85BB65, nil)
 addCurrency("Glowstone",        "minecraft:glowstone_dust", 0,   'DUST',  0xD0D000, 5)
 addCurrency("Iron ingot",  "minecraft:iron_ingot",     0,   'INGOT', 0xAAAAAA, nil)
 addCurrency("Iron block",    "minecraft:iron_block",     0,   'BLOCK', 0xAAAAAA, 6)
-addCurrency("Copper Block",      "Forestry:resourceStorage", 1,   'BLOCK', 0xA5642F, 6)
 addCurrency("Free",        nil,                        nil, nil,     0xE5E5E5, nil)
 
 return currencies
